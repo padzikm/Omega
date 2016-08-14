022ad2f1848e444fe9d57211f4a017260543806f
Michal Padzik <padzikm@gmail.com>
2016-08-14 19:42:38 +0200

    Books.Databases project

---
+++ Omega.Services/Books/Books.Databases/WindsorInstaller.cs
@@ -0,0 +1,23 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace Books.Databases
+{
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component.For<IDocumentStore>().UsingFactoryMethod(() =>
+            {
+                var dbStore = new DocumentStore()
+                {
+                    ConnectionStringName = "BooksDatabase",
+                }.Initialize(true);
+                return dbStore;
+            }));
+        }
+    }
+}
