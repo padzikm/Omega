12a33b96f27aed21de1e7f8385bcb3501f1dabb2
Michal Padzik <padzikm@gmail.com>
2016-08-14 19:44:07 +0200

    Books.DatabaseSetup project

---
+++ Omega.Services/Books/Books.DatabaseSetup/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.DatabaseSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
