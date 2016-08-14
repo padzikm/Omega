6ce5123dfc3380538a182811781d73954ef6e13e
Michal Padzik <padzikm@gmail.com>
2016-08-14 18:25:42 +0200

    Books.Service set up

---
+++ Omega.Services/Books/Books.Service/Windsor/WindsorBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace Books.Service
+{
+    class WindsorBootstrapper
+    {
+        public static IWindsorContainer Bootstrapp()
+        {
+            var container = new WindsorContainer();
+            container.Install(FromAssembly.This());
+            return container;
+        }
+    }
+}
---
+++ Omega.Services/Books/Books.Service/Windsor/WindsorInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Books.Service.Windsor
+{
+    class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            
+        }
+    }
+}
